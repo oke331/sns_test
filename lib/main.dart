379117d13bc9c9a39875404eb977a25e285fa5a1
oke331 <takeo.takeo331@gmail.com>
2021-02-03 23:21:10 +0900

    googleサインインできるようにした

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
@@ -40,79 +43,143 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            if (_auth == null) Text('ログイン状態：未ログイン'),
-            if (_auth != null && _auth.currentUser.isAnonymous)
-              Text('ログイン状態：匿名ユーザー'),
-            if (_auth != null && _auth.currentUser.displayName != null)
-              Text(
-                  'ログイン状態：Googleログイン中\nユーザー名：${_auth.currentUser.displayName}'),
-            ElevatedButton(
-              child: Text('匿名ログイン'),
-              onPressed: () async {
-                await _auth.signInAnonymously();
-              },
+      body: StreamBuilder<User>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                if (!snapshot.hasData || snapshot.data == null)
+                  Text('ログイン状態：未ログイン'),
+                if (snapshot.data != null && snapshot.data.isAnonymous)
+                  Text('ログイン状態：匿名ユーザー\nユーザーID：${snapshot.data.uid}'),
+                if (snapshot.data != null && !snapshot.data.isAnonymous)
+                  Text('ログイン状態：Googleログイン中\nユーザーID：${snapshot.data.uid}'),
+                SizedBox(height: 30),
+                ElevatedButton(
+                  child: Text('匿名ログイン'),
+                  onPressed: () async {
+                    await _auth.signInAnonymously();
+                  },
+                ),
+                ElevatedButton(
+                  child: Text('Google ログイン'),
+                  onPressed: () async {
+                    await _handleGoogleSignIn(context);
+                  },
+                ),
+                ElevatedButton(
+                  child: Text('Google ログイン & 匿名紐付け'),
+                  onPressed: () async {
+                    await _handleGoogleSignInAndConnectAnonymous(context);
+                  },
+                ),
+                SizedBox(height: 30),
+                ElevatedButton(
+                  child: Text('サインアウト'),
+                  onPressed: () async {
+                    _handleSignOut(context);
+                  },
+                  style: ElevatedButton.styleFrom(primary: Colors.red),
+                ),
+                SizedBox(height: 30),
+                ElevatedButton(
+                  child: Text('日付とユーザー書き込み'),
+                  onPressed: () async {
+                    await _setUserIdAndDateTime(context);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.yellow,
+                    onPrimary: Colors.black,
+                  ),
+                )
+              ],
             ),
-            ElevatedButton(
-              child: Text('Google ログイン'),
-              onPressed: () async {
-                await _handleGoogleSignIn();
-              },
-            ),
-            ElevatedButton(
-              child: Text('匿名ユーザーサインアウト'),
-              onPressed: () async {
-                if (_auth != null && _auth.currentUser.isAnonymous) {
-                  await _auth.signOut();
-                }
-              },
-            ),
-            ElevatedButton(
-              child: Text('Googleユーザーサインアウト'),
-              onPressed: () async {
-                _handleGoogleSignOut();
-              },
-            ),
-            ElevatedButton(
-              child: Text('日付とユーザー書き込み'),
-              onPressed: () async {
-                await _setUserIdAndDateTime(context);
-              },
-            )
-          ],
-        ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.autorenew),
+        onPressed: () => setState(() {}),
       ),
     );
   }
 
-  Future<void> _handleGoogleSignIn() async {
-    GoogleSignInAccount googleUser = await _googleSignIn.signIn();
-    GoogleSignInAuthentication googleAuth = await googleUser.authentication;
-    final AuthCredential credential = GoogleAuthProvider.credential(
-        idToken: googleAuth.idToken, accessToken: googleAuth.accessToken);
-    await _auth.signInWithCredential(credential);
-    setState(() {});
+  Future<void> _handleGoogleSignIn(BuildContext context) async {
+    try {
+      GoogleSignInAccount googleUser = await _googleSignIn.signIn();
+      GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+      final AuthCredential credential = GoogleAuthProvider.credential(
+          idToken: googleAuth.idToken, accessToken: googleAuth.accessToken);
+      await _auth.signInWithCredential(credential);
+    } on Exception catch (e) {
+      final snackBar = SnackBar(content: Text('おっと失敗したようだ'));
+      Scaffold.of(context).showSnackBar(snackBar);
+      print(e);
+    }
   }
 
-  Future<void> _handleGoogleSignOut() async {
-    await _auth.signOut();
-    if (_googleSignIn != null && _googleSignIn.currentUser != null) {
-      await _googleSignIn.signOut();
+  Future<void> _handleGoogleSignInAndConnectAnonymous(
+      BuildContext context) async {
+    try {
+      GoogleSignInAccount googleUser = await _googleSignIn.signIn();
+      GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+      final AuthCredential credential = GoogleAuthProvider.credential(
+          idToken: googleAuth.idToken, accessToken: googleAuth.accessToken);
+      await _auth.currentUser.linkWithCredential(credential);
+      final snackBar = SnackBar(content: Text('リンク完了したかもだぜぇ？'));
+      Scaffold.of(context).showSnackBar(snackBar);
+    } on Exception catch (e) {
+      final snackBar = SnackBar(content: Text('おっと失敗したようだ'));
+      Scaffold.of(context).showSnackBar(snackBar);
+      print(e);
+    }
+  }
+
+  Future<void> _handleSignOut(BuildContext context) async {
+    try {
+      await _auth.signOut();
+      if (_googleSignIn != null && _googleSignIn.currentUser != null) {
+        await _googleSignIn.signOut();
+      }
+      final snackBar = SnackBar(content: Text('サインアウト成功だぜぇ？'));
+      Scaffold.of(context).showSnackBar(snackBar);
+    } on Exception catch (e) {
+      final snackBar = SnackBar(content: Text('おっと失敗したようだ'));
+      Scaffold.of(context).showSnackBar(snackBar);
+      print(e);
     }
   }
 
   Future<void> _setUserIdAndDateTime(BuildContext context) async {
-    if (_auth == null) {
-      final snackBar = SnackBar(content: Text('認証情報がありません！'));
+    if (_auth == null || _auth.currentUser == null) {
+      final snackBar = SnackBar(content: Text('認証情報がないぜぇ？'));
       Scaffold.of(context).showSnackBar(snackBar);
       return;
     }
-    final fireStore = FirebaseFirestore.instance;
-    await fireStore.collection('user').doc(_auth.currentUser.uid).set({
-      'datetime': DateTime.now(),
-    }, SetOptions(merge: false));
+    try {
+      final fireStore = FirebaseFirestore.instance;
+      await fireStore
+          .collection('user')
+          .doc(_auth.currentUser.uid)
+          .collection(DateTime.now().toString())
+          .doc()
+          .set({
+        'test': true,
+      }, SetOptions(merge: false));
+      final snackBar = SnackBar(content: Text('書き込み完了だぜぇ？'));
+      Scaffold.of(context).showSnackBar(snackBar);
+    } on Exception catch (e) {
+      final snackBar = SnackBar(content: Text('おっと失敗したようだ'));
+      Scaffold.of(context).showSnackBar(snackBar);
+      print(e);
+    }
   }
 }
