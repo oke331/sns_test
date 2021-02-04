df29fe0ac3f6421c383e6d6efaeffd44e88dc4e8
oke331 <takeo.takeo331@gmail.com>
2021-02-04 23:20:44 +0900

    メールログインできるよう変更

--- lib/main.dart
+++ lib/main.dart
@@ -36,6 +36,8 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   final _googleSignIn = new GoogleSignIn();
   final _auth = FirebaseAuth.instance;
+  String email;
+  String pass;
 
   @override
   Widget build(BuildContext context) {
@@ -44,7 +46,7 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: StreamBuilder<User>(
-        stream: FirebaseAuth.instance.authStateChanges(),
+        stream: FirebaseAuth.instance.userChanges(),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return Center(
@@ -54,54 +56,122 @@ class _MyHomePageState extends State<MyHomePage> {
 
           return Center(
             child: SingleChildScrollView(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  if (!snapshot.hasData || snapshot.data == null)
-                    Text('ログイン状態：未ログイン'),
-                  if (snapshot.data != null && snapshot.data.isAnonymous)
-                    Text('ログイン状態：匿名ユーザー\nユーザーID：${snapshot.data.uid}'),
-                  if (snapshot.data != null && !snapshot.data.isAnonymous)
-                    Text('ログイン状態：Googleログイン中\nユーザーID：${snapshot.data.uid}'),
-                  SizedBox(height: 30),
-                  ElevatedButton(
-                    child: Text('匿名ログイン'),
-                    onPressed: () async {
-                      await _auth.signInAnonymously();
-                    },
-                  ),
-                  ElevatedButton(
-                    child: Text('Google ログイン'),
-                    onPressed: () async {
-                      await _handleGoogleSignIn(context);
-                    },
-                  ),
-                  ElevatedButton(
-                    child: Text('Google ログイン & 匿名紐付け'),
-                    onPressed: () async {
-                      await _handleGoogleSignInAndConnectAnonymous(context);
-                    },
-                  ),
-                  SizedBox(height: 30),
-                  ElevatedButton(
-                    child: Text('サインアウト'),
-                    onPressed: () async {
-                      _handleSignOut(context);
-                    },
-                    style: ElevatedButton.styleFrom(primary: Colors.red),
-                  ),
-                  SizedBox(height: 30),
-                  ElevatedButton(
-                    child: Text('日付とユーザー書き込み'),
-                    onPressed: () async {
-                      await _setUserIdAndDateTime(context);
-                    },
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.yellow,
-                      onPrimary: Colors.black,
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    if (!snapshot.hasData || snapshot.data == null)
+                      Text('ログイン状態：未ログイン'),
+                    SizedBox(height: 30),
+                    if (snapshot.hasData && snapshot.data.isAnonymous)
+                      Text('ログイン状態：匿名ログイン'),
+                    if (snapshot.hasData)
+                      for (UserInfo user in snapshot.data.providerData)
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 30),
+                          child: Text(
+                              'ログイン状態：${user.providerId}\nuid：${user.uid}\nemail:${user.email}'),
+                        ),
+                    if (snapshot.hasData)
+                      Text('Firebase上のuid：${snapshot.data.uid}'),
+                    SizedBox(height: 30),
+                    ElevatedButton(
+                      child: Text('匿名ログイン'),
+                      onPressed: () async {
+                        try {
+                          await _auth.signInAnonymously();
+                        } on Exception catch (e) {
+                          final snackBar =
+                              SnackBar(content: Text('おっと失敗したようだ'));
+                          Scaffold.of(context).showSnackBar(snackBar);
+                          print(e);
+                        }
+                      },
                     ),
-                  )
-                ],
+                    ElevatedButton(
+                      child: Text('Google ログイン'),
+                      onPressed: () async {
+                        await _handleGoogleSignIn(context);
+                      },
+                    ),
+                    ElevatedButton(
+                      child: Text('Google紐付け'),
+                      onPressed: () async {
+                        await _handleGoogleSignInAndLink(context);
+                      },
+                    ),
+                    SizedBox(height: 20),
+                    TextField(
+                      decoration: InputDecoration(labelText: 'email'),
+                      onChanged: (val) {
+                        email = val;
+                      },
+                    ),
+                    TextField(
+                      decoration: InputDecoration(labelText: 'pass'),
+                      onChanged: (val) {
+                        pass = val;
+                      },
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        ElevatedButton(
+                          child: Text('サインアップ'),
+                          onPressed: () async {
+                            await _signUp(context);
+                          },
+                          style: ElevatedButton.styleFrom(primary: Colors.red),
+                        ),
+                        ElevatedButton(
+                          child: Text('ログイン'),
+                          onPressed: () async {
+                            await _login(context);
+                          },
+                          style: ElevatedButton.styleFrom(primary: Colors.red),
+                        ),
+                        ElevatedButton(
+                          child: Text('メール紐付け'),
+                          onPressed: () async {
+                            await _handleEmailSignInAndLink(context);
+                          },
+                          style: ElevatedButton.styleFrom(primary: Colors.red),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 30),
+                    ElevatedButton(
+                      child: Text('日付とユーザー書き込み'),
+                      onPressed: () async {
+                        await _setUserIdAndDateTime(context);
+                      },
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.yellow,
+                        onPrimary: Colors.black,
+                      ),
+                    ),
+                    SizedBox(height: 30),
+                    ElevatedButton(
+                      child: Text('サインアウト'),
+                      onPressed: () async {
+                        await _handleSignOut(context);
+                      },
+                      style: ElevatedButton.styleFrom(primary: Colors.pink),
+                    ),
+                    SizedBox(height: 30),
+                    ElevatedButton(
+                      child: Text('Googleのリンク解除'),
+                      onPressed: () async {
+                        await _unlink(context);
+                      },
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.green,
+                        onPrimary: Colors.white,
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
           );
@@ -128,10 +198,9 @@ class _MyHomePageState extends State<MyHomePage> {
     }
   }
 
-  Future<void> _handleGoogleSignInAndConnectAnonymous(
-      BuildContext context) async {
+  Future<void> _handleGoogleSignInAndLink(BuildContext context) async {
     if (_auth.currentUser == null) {
-      final snackBar = SnackBar(content: Text('匿名ログインしてからにしてくれい'));
+      final snackBar = SnackBar(content: Text('ログインしてからにしてくれい'));
       Scaffold.of(context).showSnackBar(snackBar);
       return;
     }
@@ -144,9 +213,14 @@ class _MyHomePageState extends State<MyHomePage> {
       await _auth.currentUser.linkWithCredential(credential);
       final snackBar = SnackBar(content: Text('リンク完了したかもだぜぇ？'));
       Scaffold.of(context).showSnackBar(snackBar);
-    } on Exception catch (e) {
-      final snackBar = SnackBar(content: Text('おっと失敗したようだ'));
-      Scaffold.of(context).showSnackBar(snackBar);
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'credential-already-in-use') {
+        final snackBar = SnackBar(content: Text('エラー：すでに紐づいているとのこと'));
+        Scaffold.of(context).showSnackBar(snackBar);
+      } else {
+        final snackBar = SnackBar(content: Text('おっと失敗したようだ'));
+        Scaffold.of(context).showSnackBar(snackBar);
+      }
       print(e);
     }
   }
@@ -187,6 +261,78 @@ class _MyHomePageState extends State<MyHomePage> {
     } on Exception catch (e) {
       final snackBar = SnackBar(content: Text('おっと失敗したようだ'));
       Scaffold.of(context).showSnackBar(snackBar);
+      print(e);
+    }
+  }
+
+  Future<void> _unlink(BuildContext context) async {
+    if (_auth.currentUser == null) {
+      final snackBar = SnackBar(content: Text('認証情報がないぜぇ？'));
+      Scaffold.of(context).showSnackBar(snackBar);
+      return;
+    }
+    try {
+      final providerData = _auth.currentUser.providerData;
+      for (UserInfo userInfo in providerData) {
+        if (userInfo.providerId == 'google.com') {
+          _auth.currentUser.unlink(userInfo.providerId);
+          await _googleSignIn.signOut();
+          final snackBar = SnackBar(content: Text('Googleのunlink完了だぜぇ？'));
+          Scaffold.of(context).showSnackBar(snackBar);
+        }
+      }
+    } on Exception catch (e) {
+      final snackBar = SnackBar(content: Text('おっと失敗したようだ'));
+      Scaffold.of(context).showSnackBar(snackBar);
+      print(e);
+    }
+  }
+
+  Future<void> _signUp(BuildContext context) async {
+    if (email == null && pass == null && pass.length > 5) {
+      final snackBar = SnackBar(content: Text('ちゃんと入力して！'));
+      Scaffold.of(context).showSnackBar(snackBar);
+    }
+    try {
+      await _auth.createUserWithEmailAndPassword(email: email, password: pass);
+    } on Exception catch (e) {
+      final snackBar = SnackBar(content: Text('おっと失敗したようだ'));
+      Scaffold.of(context).showSnackBar(snackBar);
+      print(e);
+    }
+  }
+
+  Future<void> _login(BuildContext context) async {
+    try {
+      await _auth.signInWithEmailAndPassword(email: email, password: pass);
+    } on Exception catch (e) {
+      final snackBar = SnackBar(content: Text('おっと失敗したようだ'));
+      Scaffold.of(context).showSnackBar(snackBar);
+      print(e);
+    }
+  }
+
+  Future<void> _handleEmailSignInAndLink(BuildContext context) async {
+    if (_auth.currentUser == null) {
+      final snackBar = SnackBar(content: Text('ログインしてからにしてくれい'));
+      Scaffold.of(context).showSnackBar(snackBar);
+      return;
+    }
+
+    try {
+      final AuthCredential credential =
+          EmailAuthProvider.credential(email: email, password: pass);
+      await _auth.currentUser.linkWithCredential(credential);
+      final snackBar = SnackBar(content: Text('リンク完了したかもだぜぇ？'));
+      Scaffold.of(context).showSnackBar(snackBar);
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'credential-already-in-use') {
+        final snackBar = SnackBar(content: Text('エラー：すでに紐づいているとのこと'));
+        Scaffold.of(context).showSnackBar(snackBar);
+      } else {
+        final snackBar = SnackBar(content: Text('おっと失敗したようだ'));
+        Scaffold.of(context).showSnackBar(snackBar);
+      }
       print(e);
     }
   }
